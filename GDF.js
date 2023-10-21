cd52b7529b9c918217363f48a8b86d581f02ed96
Foxglove <mrbluesphere@gmail.com>
2023-10-21 18:06:37 -0400

    Mer

--- GDF.js
+++ GDF.js
@@ -1,17 +1,24 @@
 
+/* ----------------------- Preliminary ----------------------- */
 
+/* ----------------------- Header Stuff ----------------------- */
 
+/* ----------------------- Body Stuff ----------------------- */
 
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
     const getDownFlooring = document.querySelector('#GetDownFlooring');
+    const logo = this.document.querySelector('.logo');
 
-    if (window.scrollY > 100) {
-        getDownFlooring.textContent = 'GDF';
-        getDownFlooring.style.fontSize = '20px';
+    if (window.scrollY > 200) {
+        getDownFlooring.style.fontSize = '25px';
+        logo.style.height = '1.8rem';
+
+
         //if scroll opposite, reset:
     } else {
-        getDownFlooring.style.fontSize = '40px';
+        getDownFlooring.style.fontSize = '42px';
         getDownFlooring.textContent = 'Get Down Flooring, Inc.';
+        logo.style.height = '4rem';
     }
 });

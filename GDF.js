7607585796c9ea38ad8e28c4b04cea1e3e090fec
Foxglove <mrbluesphere@gmail.com>
2023-10-25 20:46:04 -0400

    Mer

--- GDF.js
+++ GDF.js
@@ -22,3 +22,40 @@ window.addEventListener('scroll', function() {
         logo.style.height = '4rem';
     }
 });
+
+// ------------------------- Gallery Stuff -------------------------
+
+
+//  functions declared
+let slideIndex = 1;
+  showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
+  
